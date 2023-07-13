65f475a9c507fd69b2f4f05c5d0b2e525cd610b5
Евгений Бученков <bieyffull@gmail.com>
2023-07-13 20:56:11 +0300

    Added DI

---
+++ ShapeLibrary/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.Design;
+using Microsoft.Extensions.DependencyInjection;
+using ShapeLibrary.Application.Interfaces;
+
+namespace ShapeLibrary.Application;
+
+public static class DependencyInjection
+{
+    public static void AddShapeLibrary(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddTransient<IShapeFactory, ShapeFactory>();
+    }
+}
